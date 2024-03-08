9865770d69f5b3bcd50c3869a1a7246bbaee26c4
Hyunji <115874801+hjjj99@users.noreply.github.com>
2024-03-08 23:40:27 +0900

    [Bronze I] Title: 약수, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		long[] arr = new long[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = Long.parseLong(st.nextToken());
+		}
+		
+        //정렬
+		Arrays.sort(arr);
+		
+		System.out.println(arr[0] * arr[N - 1]);
+	}
+
+}

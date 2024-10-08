8a7de27db25775cb1f5e0d80069014bd3e5cd6f4
Hyunji <115874801+hjjj99@users.noreply.github.com>
2024-10-08 23:47:46 +0900

    [Bronze IV] Title: 주사위, Time: 112 ms, Memory: 14612 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 주사위를 두 번 던져서 합을 구한다.
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int num = Integer.parseInt(br.readLine());
+        int answer = 0;
+
+        for (int i = 1; i <= num; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int numA = Integer.parseInt(st.nextToken());
+            int numB = Integer.parseInt(st.nextToken());
+            answer = numA + numB;
+
+            sb.append("Case ").append(i).append(": ").append(answer).append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
